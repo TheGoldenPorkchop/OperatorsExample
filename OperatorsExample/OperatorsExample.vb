974b57807218ad2da84cfcb574f720cbaa818944
TheGoldenPorkchop <angelnavamusic@gmail.com>
2025-01-16 08:51:57 -0700

    Does basic math operations

--- OperatorsExample/OperatorsExample.vb
+++ OperatorsExample/OperatorsExample.vb
@@ -1,6 +1,14 @@
-﻿Module OperatorsExample
+﻿'Angel Nava
+'Spring 2025
+'RCET2265
+'OperatorsExample
+'https://github.com/TheGoldenPorkchop/OperatorsExample.git
+
+Module OperatorsExample
 
     Sub Main()
+        Console.Write("5 + 3 = ") 'prints a string
+        Console.WriteLine(5 + 3) 'prints the sum
 
     End Sub
 
