d35e317de43c33c59887827f9b499a0bbdc9ca7f
TheGoldenPorkchop <angelnavamusic@gmail.com>
2025-01-17 08:53:30 -0700

    Added integer quotient operator, floating point quotient operator, modulus operator, exponent operator, and concatenated some strings. As well as checking out new commands like vbNewLine and StrDup.

--- OperatorsExample/OperatorsExample.vb
+++ OperatorsExample/OperatorsExample.vb
@@ -14,17 +14,39 @@ Module OperatorsExample
 
         'difference operator
         Console.Write("5 - 3 = ") 'prints a string
-        Console.WriteLine(5 - 3) 'prints the sum
+        Console.WriteLine(5 - 3) 'prints the difference
         Console.WriteLine()
         'product operator
         Console.Write("5 * 3 = ") 'prints a string
-        Console.WriteLine(5 * 3) 'prints the sum
+        Console.WriteLine(5 * 3) 'prints the product
         Console.WriteLine()
 
-        'quotient operator
+        'floating point quotient operator
         Console.Write("5 / 3 = ") 'prints a string
-        Console.WriteLine(5 / 3 * 3) 'prints the sum
+        Console.WriteLine(5 / 3) 'prints the quotient
         Console.WriteLine()
+
+        'integer quotient operator
+        Console.Write("5 \ 3 = ") 'prints a string
+        Console.WriteLine(5 \ 3) 'prints the quotient
+        Console.WriteLine()
+
+        'modulus operator
+        Console.Write("5 mod 3 = ") 'prints a string
+        Console.WriteLine(5 Mod 3) 'prints the remainder
+        Console.WriteLine()
+
+        'exponent operator
+        Console.Write("5 ^ 3 = ") 'prints a string
+        Console.WriteLine(5 ^ 3) 'prints the exponent
+        Console.WriteLine()
+
+        'concatenation operator
+        Console.Write("5" & " " & "+" & " " & "3" & " " & "=" & " ") 'concatenates sring string
+        Console.WriteLine(5 + 3) 'prints the sum
+        Console.WriteLine()
+        Console.WriteLine("wake me up inside" & vbNewLine & "wake me up inside") 'adds a new line with concatenation
+        Console.WriteLine(StrDup(20, "*")) 'duplicates the asteroids
     End Sub
 
 End Module
