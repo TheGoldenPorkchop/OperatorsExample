c144cbd987a3baba009636abe617cf97e44f4fb6
TheGoldenPorkchop <angelnavamusic@gmail.com>
2025-01-21 08:41:46 -0700

    Added comparison operations for numbers and strings.

--- OperatorsExample/OperatorsExample.vb
+++ OperatorsExample/OperatorsExample.vb
@@ -4,6 +4,8 @@
 'OperatorsExample
 'https://github.com/TheGoldenPorkchop/OperatorsExample.git
 
+Option Compare Text
+
 Module OperatorsExample
 
     Sub Main()
@@ -47,6 +49,36 @@ Module OperatorsExample
         Console.WriteLine()
         Console.WriteLine("wake me up inside" & vbNewLine & "wake me up inside") 'adds a new line with concatenation
         Console.WriteLine(StrDup(20, "*")) 'duplicates the asteroids
+
+        'Comparisons
+        Console.Write("5 > 3 = ") 'prints a string
+        Console.WriteLine(5 > 3)
+        Console.WriteLine()
+
+        Console.Write("5 < 3 = ") 'prints a string
+        Console.WriteLine(5 < 3)
+        Console.WriteLine()
+
+        Console.Write("does 5 = 3? ") 'prints a string
+        Console.WriteLine(5 = 3)
+        Console.WriteLine()
+
+        Console.Write("is 5 not equal to 3? ") 'prints a string
+        Console.WriteLine(5 <> 3)
+        Console.WriteLine()
+
+        Console.Write("A > B = ") 'prints a string
+        Console.WriteLine("A" > "B")
+        Console.WriteLine()
+
+        Console.Write("A < B = ") 'prints a string
+        Console.WriteLine("A" < "B")
+        Console.WriteLine()
+
+        Console.Write("is " & Chr(34) & "A" & Chr(34) & " the same as " & Chr(34) & "a" & Chr(34) & " ? ") 'prints a string
+        Console.WriteLine("A" = "a")
+        Console.WriteLine()
+
     End Sub
 
 End Module
